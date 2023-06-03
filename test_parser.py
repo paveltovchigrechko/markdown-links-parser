ae9e03674b1c5efe07cca955ec831998c7bbf975
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-06-03 13:23:30 +0600

    Added unit tests for File

--- test_parser.py
+++ test_parser.py
@@ -35,7 +35,49 @@ class MarkdownLinkTests(unittest.TestCase):
 
 
 class FileTests(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.path = "path/to/file"
+        self.file = File(self.path)
+
+    def test_init(self):
+        self.assertEqual(self.file.path, "path/to/")
+        self.assertEqual(self.file.name, "file")
+        self.assertEqual(self.file.path_with_name, self.path)
+        self.assertEqual(self.file.inbound_links, {})
+        self.assertEqual(self.file.internal_links, [])
+        self.assertEqual(self.file.external_links, [])
+        self.assertEqual(self.file.outside_links, [])
+
+    def test_parse_links(self):
+        pass
+
+    def test_check_internal_links_positive(self):
+        self.file.inbound_links = {"heading1", "heading2", "heading3"}
+        self.file.internal_links = [(1, Link("#heading1")),
+                                    (10, Link("#heading2")),
+                                    (20, Link("#heading3"))]
+        broken_links = self.file.check_internal_links()
+        self.assertEqual(len(broken_links), 0)
+
+        self.file.internal_links.pop()
+        broken_links = self.file.check_internal_links()
+        self.assertEqual(len(broken_links), 0)
+
+    def test_check_internal_links_negative(self):
+        self.file.inbound_links = {"heading1", "heading2", "heading3"}
+        self.file.internal_links = [(1, Link("#heading1")),
+                                    (10, Link("#heading2")),
+                                    (30, Link("#heading4"))]
+        broken_links = self.file.check_internal_links()
+        self.assertEqual(len(broken_links), 1)
+        self.assertEqual(broken_links[0][0], 30)
+        self.assertEqual(broken_links[0][1].heading, "heading4")
+
+        deleted_heading = self.file.inbound_links.pop()
+        broken_links = self.file.check_internal_links()
+        self.assertEqual(len(broken_links), 2)
+        self.assertTrue(broken_links[0][1].heading == deleted_heading or
+                        broken_links[1][1].heading == deleted_heading)
 
 
 class DirTests(unittest.TestCase):
