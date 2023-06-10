afa1174b2d32e85bde2fd5bbd4b2d6a7936e6efd
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-06-10 15:09:22 +0600

    Added main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from parser import *
+from config import *
+
+config = set_config()
+
+dir = Dir(config["MAIN"]["root"])
+dir.scan_filenames(extension=config["MAIN"]["file_extension"])
+
+if config["MAIN"]["action"] == "check_links":
+    dir.parse_files()
+    dir.check_internal_links()
+    dir.check_external_links()
+    dir.print_broken_links()
+elif config["MAIN"]["action"] == "search":
+    str_to_find = input("Enter the string to find: ")
+    dir.search(str_to_find)
+elif config["MAIN"]["action"] == "print_links":
+    dir.parse_files()
+    dir.print_all_links()
