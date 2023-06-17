eb6ceaea71eaf64ab6b31fd27a496f4e6d3f7f35
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-06-17 18:04:23 +0600

    Rm debugging print statements

--- test_config.py
+++ test_config.py
@@ -7,7 +7,6 @@ import unittest
 
 class ConfigTests(unittest.TestCase):
     def test_read_correct_config_file(self):
-        print("test_read_correct_config_file")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.write('''[MAIN]\nroot: .\nfile_extension: .mdx\noutput: console\naction: check_links''')
 
@@ -17,12 +16,10 @@ class ConfigTests(unittest.TestCase):
         os.remove(config.CONFIG_NAME)
 
     def test_no_config_file(self):
-        print("test_no_config_file")
         cfg = config.set_config()
         self.assertEqual(cfg, config.DEFAULT_CONFIG)
 
     def test_config_file_without_sections(self):
-        print("test_config_file_without_sections")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.write('''root: .\nfile_extension: .mdx\noutput: console\naction: check_links''')
 
@@ -32,7 +29,6 @@ class ConfigTests(unittest.TestCase):
         os.remove(config.CONFIG_NAME)
 
     def test_config_file_empty(self):
-        print("test_config_file_empty")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.close()
 
@@ -42,7 +38,6 @@ class ConfigTests(unittest.TestCase):
         os.remove(config.CONFIG_NAME)
 
     def test_config_file_without_main_section(self):
-        print("test_config_file_without_main_section")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.write('''[SECTION]\nroot: .\nfile_extension: .mdx\noutput: console\naction: check_links''')
 
@@ -52,7 +47,6 @@ class ConfigTests(unittest.TestCase):
         os.remove(config.CONFIG_NAME)
 
     def test_config_file_without_obligatory_keys(self):
-        print("test_config_file_without_obligatory_keys")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.write('''[MAIN]\nparameter: .\nextension: .mdx\noutput_result: file\nfunc_action: check_links''')
 
@@ -64,7 +58,6 @@ class ConfigTests(unittest.TestCase):
         os.remove(config.CONFIG_NAME)
 
     def test_config_file_with_wrong_value_in_enum_key(self):
-        print("test_config_file_with_wrong_enum__in_obligatory_key")
         with open(config.CONFIG_NAME, mode='w') as config_file:
             config_file.write('''[MAIN]\nroot: .\nfile_extension: .mdx\noutput: print\naction: value''')
 
