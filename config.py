44e8115bfa8cc15b3039acc2fa24a4827f159b93
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-06-17 15:35:20 +0600

    Small changes

--- config.py
+++ config.py
@@ -37,10 +37,20 @@ def set_config():
         else:
             for obligatory_key in DEFAULT_CONFIG["MAIN"]:
                 if obligatory_key not in config["MAIN"]:
-                    print(f"Missing obligatory {obligatory_key} "
-                          f"parameter: using value from default configuration ({DEFAULT_CONFIG['MAIN'][obligatory_key]})")
+                    print(f"Missing obligatory {obligatory_key} parameter: "
+                          f"using value from default configuration ({DEFAULT_CONFIG['MAIN'][obligatory_key]})")
                     config["MAIN"][obligatory_key] = DEFAULT_CONFIG['MAIN'][obligatory_key]
-
+                elif obligatory_key == "action" or obligatory_key == "output":
+                    value = config["MAIN"][obligatory_key]
+                    print(value)
+                    action_values = [item.value for item in Action]
+                    print(action_values)
+                    output_values = [item.value for item in Output]
+                    print(output_values)
+                    if value not in action_values or value not in output_values:
+                        print(f"Key {obligatory_key} has incorrect value, "
+                              f"using value from default configuration ({DEFAULT_CONFIG['MAIN'][obligatory_key]})")
+                        config["MAIN"][obligatory_key] = DEFAULT_CONFIG['MAIN'][obligatory_key]
     return config
 
 if __name__ == "__main__":
